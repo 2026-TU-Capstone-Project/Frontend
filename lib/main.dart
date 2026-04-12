1422b3f23efccdcfa1ea0e729ae9053404d74ab2
songjeonghun <songzh0415@naver.com>
2026-04-12 12:35:16 +0900

    04/12 한글 plugin

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:capstone_fe/user/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 Future<void> main() async {
@@ -22,6 +23,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final baseTextTheme = GoogleFonts.notoSansKrTextTheme(
+      Theme.of(context).textTheme,
+    );
+    const emojiFallback = [
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Noto Color Emoji',
+    ];
+
     return MaterialApp(
       title: '다이버바',
       navigatorKey: navigatorKey,
@@ -29,6 +39,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         platform: Platform.isIOS ? TargetPlatform.iOS : TargetPlatform.android,
+        textTheme: baseTextTheme.apply(fontFamilyFallback: emojiFallback),
         progressIndicatorTheme: const ProgressIndicatorThemeData(
           color: AppColors.PRIMARYCOLOR,
           circularTrackColor: AppColors.BORDER_COLOR,
